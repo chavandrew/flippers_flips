e12669a48efddd818ccef61c240ab4707706ef6c
Andrew Chavez <chavandrew@gmail.com>
2017-11-24 10:28:35 -0600

    added links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import{
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div>
+
+
+    <Router>
+        <div>
+          <nav className='tabs'>
+            <NavLink exact to='/'>Home</NavLink>{' '}
+            <NavLink to='/about'>About</NavLink>{' '}
+            <NavLink to='/foodmenu'>Food Menu</NavLink>{' '}
+          </nav>
+
+        </div>
+      </Router>
+
+
+
+
+
+
+</div>
+);
+
+
   }
 }
 
